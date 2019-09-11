88160103ab02f4ab86b6b6fbcfc528cb54fd4b57
focused220 <roblhjr.220@gmail.com>
2019-09-11 11:11:36 -0700

    updated port setting

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express'); 
 const server = require('./server');
+const port = process.env.PORT || 4000;
 
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(port, () => {
+  console.log(`\n*** Server Running on ${port} ***\n`);
 });
